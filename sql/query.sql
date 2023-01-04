1435c064f69367d86d8d79e735617bd05ddb9713
Julian Montoya <jdmontoyavallejo@gmail.com>
2023-01-04 15:31:01 +0000

    Testing format

---
+++ sql/query.sql
@@ -0,0 +1,17 @@
+``` sql
+SELECT 
+  product_manufacturer, 
+  product_name, 
+  COUNT(*) as unk_pm 
+FROM 
+  `inventory-control-managment.inventorydataset.inventory_table` 
+WHERE 
+  product_manufacturer = 'Unknown' 
+GROUP BY 
+  product_manufacturer, 
+  product_name 
+ORDER BY 
+  product_manufacturer DESC;
+```
+
+
