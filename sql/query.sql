0cabce04cd92759e3f31e1f4f910500bc1597921
Julian Montoya <jdmontoyavallejo@gmail.com>
2023-01-04 16:18:26 +0000

    Format table test

--- sql/query.sql
+++ sql/query.sql
@@ -13,3 +13,9 @@ ORDER BY
   product_manufacturer DESC;
 
 -- Query result
+
+| product_manufacturer | product_name             | unk_pm |
+|----------------------|--------------------------|--------|
+| Unknown              | Towels                   | 8      |
+| Unknown              | Box of 100 Waxing Strips | 1      |
+| Unknown              | Eye Protection           | 1      |
