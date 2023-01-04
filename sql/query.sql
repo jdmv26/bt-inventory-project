25287485b8ce1ae089d2b018cde14e8f65db5251
Julian Montoya <jdmontoyavallejo@gmail.com>
2023-01-04 16:50:31 +0000

    Added sql query results

--- sql/query.sql
+++ sql/query.sql
@@ -101,3 +101,104 @@ LIMIT
 | Towels       | 0             | Unknown              | Medium | 9        | White  | Unknown      | Good   |
 | Towels       | 0             | Unknown              | Small  | 8        | White  | Unknown      | Good   |
 | Towels       | 0             | Unknown              | Large  | 8        | Grey   | Unknown      | Good   |
+
+
+-- Q5 What’s the most expensive item?
+
+SELECT 
+  * 
+FROM 
+  `inventory-control-managment.inventorydataset.inventory_table` 
+ORDER BY 
+  product_price DESC 
+LIMIT 
+  1
+
+-- Query result
+
+| product_name           | product_price | product_manufacturer | size | quantity | colour | where_bought | status |
+|------------------------|---------------|----------------------|------|----------|--------|--------------|--------|
+| Box of 25 Return Plate | 96            | Lumenis              | N/A  | 2        | N/A    | Lumenis      | Low    |
+
+
+-- Q6 Which items require attention i.e low in stock?
+
+SELECT 
+  * 
+FROM 
+  `inventory-control-managment.inventorydataset.inventory_table` 
+WHERE 
+  status = 'Low' 
+ORDER BY 
+  product_price DESC
+
+-- Query result
+
+| product_name                           | product_price | product_manufacturer  | size   | quantity | colour | where_bought              | status |
+|----------------------------------------|---------------|-----------------------|--------|----------|--------|---------------------------|--------|
+| Box of 25 Return Plate                 | 96            | Lumenis               | N/A    | 2        | N/A    | Lumenis                   | Low    |
+| Moisturising Essence                   | 60            | Clinicare             | 500ml  | 1        | N/A    | 4T Medical                | Low    |
+| Body and Face Moisturising Cream       | 60            | NuRevital             | 500ml  | 1        | N/A    | Lumenis                   | Low    |
+| 2 in 1 Gel Vitamin C                   | 45            | Clinicare             | 500ml  | 1        | N/A    | 4T Medical                | Low    |
+| Box of 10 Peel Off Wax                 | 41.59         | Casmara               | N/A    | 1        | Gold   | Chris and Sons            | Low    |
+| Instant Painless Peel                  | 35            | Clinicare             | 250ml  | 2        | N/A    | 4T Medical                | Low    |
+| ECG Gel                                | 31            | Camcare Health        | 5kg    | 2        | N/A    | Camcare Health            | Low    |
+| Skin Toner                             | 28.9          | Clinicare             | 500ml  | 2        | N/A    | 4T Medical                | Low    |
+| Depilatory Oil                         | 23.88         | Cirepil               | 1l     | 2        | N/A    | Ellisons                  | Low    |
+| Cleansing Lotion                       | 21.5          | Clinicare             | 500ml  | 1        | N/A    | 4T Medical                | Low    |
+| Box of 100 Razors                      | 20.34         | Justlife              | N/A    | 2        | N/A    | Aesthetic Beauty Supplies | Low    |
+| Box of 5 X3M Pure Pore Minimiser Masks | 20            | Clinicare             | N/A    | 1        | N/A    | 4T Medical                | Low    |
+| Rose Water                             | 17.4          | Strictly Professional | 4l     | 1        | N/A    | Chris and Sons            | Low    |
+| Box of 200 Gloves                      | 14.47         | sempercare            | Small  | 1        | Blue   | Amazon                    | Low    |
+| Peelable Wax                           | 13.86         | Outback Organics      | 800g   | 1        | Jade   | Ellisons                  | Low    |
+| Sunshield Care                         | 12.5          | Clinicare             | 50ml   | 1        | N/A    | 4T Medical                | Low    |
+| Ultrasound Gel                         | 10            | Anagel                | 5l     | 0        | N/A    | Anagel                    | Low    |
+| Box of 6 Tissues                       | 9.19          | Kleenex               | N/A    | 0        | N/A    | Costco                    | Low    |
+| Box of 100 Gloves                      | 6.39          | TouchFlex             | Medium | 1        | Blue   | Amazon                    | Low    |
+| Box of 100 Waxing Strips               | 5.89          | Unknown               | N/A    | 1        | Pink   | Ebay                      | Low    |
+| Nail Polish Remover                    | 2.76          | Pro Impressions       | 125ml  | 1        | N/A    | Pro Impressions           | Low    |
+
+
+-- Q7 What is the average price of the listed items?
+
+SELECT 
+  AVG(product_price) as average_price 
+FROM 
+  `inventory-control-managment.inventorydataset.inventory_table`
+
+
+-- Query result
+
+| average_price |
+|---------------|
+| 18.97117647   |
+
+
+-- Q8 Where are most of the products bought from?
+
+SELECT 
+  where_bought, 
+  COUNT(*) as cw_b 
+FROM 
+  `inventory-control-managment.inventorydataset.inventory_table` 
+GROUP BY 
+  where_bought 
+ORDER BY 
+  cw_b DESC
+
+-- Query result
+
+| where_bought              | cw_b |
+|---------------------------|------|
+| Unknown                   | 9    |
+| 4T Medical                | 7    |
+| Amazon                    | 5    |
+| Lumenis                   | 3    |
+| Chris and Sons            | 2    |
+| Ellisons                  | 2    |
+| Aesthetic Beauty Supplies | 1    |
+| Ebay                      | 1    |
+| Costco                    | 1    |
+| Pro Impressions           | 1    |
+| Anagel                    | 1    |
+| Camcare Health            | 1    |
